6d19ab1c667e61168120704cddaa67ee1fa940cf
Patrick <porchewellness@gmail.com>
2018-01-25 12:12:12 -0800

    adds redux

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
-import Home from "../client/components/Home";
+import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Routes from '../client/Routes';
 
-export default () => {
-  const content = renderToString(<Home />);
+export default (request, store) => {
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={request.path} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
+  );
 
   return `
     <html>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (request, response) => {
-  response.send(renderer());
+app.get('*', (request, response) => {
+  const store = createStore();
+
+  // Some logic to initialize
+  // and load data into the store
+
+  response.send(renderer(request, store));
 });
 
 app.listen(3000, () => {
